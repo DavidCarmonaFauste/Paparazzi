cfbd03f2cfd61e484b46b03e82a96b2fab447c63
Ignacio Ory <iory@ucm.es>
2018-04-08 20:17:13 +0200

    GameManager
    Cambios de escena:
    
    - Falta ampliarlo con todas las escenas que habrá, pero la base está hecha. Luego si puedo comento más el código.
    
    Sistema de usuarios:
    
    - 1 archivo con los nombres de los usuarios, y uno para cada uno, tengo la estructura hecha en papel, luego o mañana si puedo la implemento.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +8,8 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour {
 
     public int camaras, carretes, loot;
+
+	private bool Nivel1S, Nivel2S, Nivel3S;
 
 	public static GameManager instance = null;
 
@@ -18,6 +21,26 @@ public class GameManager : MonoBehaviour {
 			DontDestroyOnLoad(this.gameObject);
 		}
 		else Destroy(this.gameObject);
+	}
+
+	public Scene SiguienteEscena()
+	{
+		string actual;
+		Scene siguiente = SceneManager.GetActiveScene(); //Lo pongo como la actual porque hay que poner esto, o un caso default
+
+		actual = SceneManager.GetActiveScene ().name;
+
+		switch (actual) 
+		{
+		case ("Nivel1"):
+			siguiente = SceneManager.GetSceneByName ("Minijuego");
+			break;
+		case ("Minijuego"):
+			siguiente = SceneManager.GetSceneByName ("Nivel1"); //Está puesto nivel 1, hay que hacer otra escena con botones y tal a modo de transición, con la de puntuaciones y tal
+			break;
+		}
+
+		return siguiente;
 	}
 
     public int Bombillas()
@@ -34,4 +57,72 @@ public class GameManager : MonoBehaviour {
     {
         return loot;
     }
+
+	public void GuardaUsuarios (string username)
+	{
+		StreamReader entrada;
+		StreamWriter salida;
+		salida = new StreamWriter ("users");
+		string[] backup;
+		backup = new string[150];
+		int i = 0, a = 0;
+
+		if (!File.Exists ("users")) 
+		{
+			salida.WriteLine ("Lista de usuarios de Paparazzi Infilitration");
+			salida.WriteLine ();
+			salida.Close ();
+		} 
+
+		else 
+		{
+			entrada = new StreamReader ("users");
+
+			while (i < backup.Length - 1) 
+			{
+				backup [i] = entrada.ReadLine ();
+				i++;
+			}
+
+			while (backup [a] != null) 
+			{
+				salida.WriteLine (backup [a]);
+				a++;
+			}
+
+			salida.WriteLine (username);
+			salida.Close ();
+		}
+
+	}
+
+	public string CargaUsuarios(string username)
+	{
+		StreamReader entrada;
+		entrada = new StreamReader ("users");
+		bool encontrado = false;
+
+		while (!entrada.EndOfStream && !encontrado) 
+		{
+			if (username == entrada.ReadLine ()) 
+			{
+				encontrado = true;
+			}
+		}
+
+		if (!encontrado)
+			username = "error";
+		
+		return username;
+	}
+
+	public void GuardaArchivo ()
+	{
+		
+	}
+
+	public void CargaArchivo (string nombrearchivo)
+	{
+		
+	}
 }
