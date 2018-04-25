b73beee44af10eb26da40fd49d500ec075e74be1
DavidCarmonaFauste <davicarm@ucm.es>
2018-04-25 20:56:33 +0200

    Escena "TE HAN PILLADO" añadida, a falta de cargarla cuando te pillan los guardias

--- Assets/Scripts/Menus/MenuPrincipal.cs
+++ Assets/Scripts/Menus/MenuPrincipal.cs
@@ -3,17 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MenuPrincipal : MonoBehaviour {
+public class MenuPrincipal : MonoBehaviour
+{
     public string nivel1, nivel2, nivel3;
 
     public void NewGame()
     {
         SceneManager.LoadScene(nivel1);
     }
-	
+
     public void Quit()
     {
         Debug.Log("Se ha salido del videojuego.");
         Application.Quit();
+    }
+    public void Nivel1()
+    {
+        SceneManager.LoadScene(nivel1);
     }
 }
