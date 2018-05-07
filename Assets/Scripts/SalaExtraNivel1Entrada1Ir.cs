ab6c1c455639aa58bbdfa7a545c2453eb6a66f62
pablojor <pablojor@ucm.es>
2018-05-07 15:55:38 +0200

    Añadidas oscuridades que faltaban

--- Assets/Scripts/SalaExtraNivel1Entrada1Ir.cs
+++ Assets/Scripts/SalaExtraNivel1Entrada1Ir.cs
@@ -7,7 +7,6 @@ public class SalaExtraNivel1Entrada1Ir : MonoBehaviour {
 
     private void OnCollisionEnter2D (Collision2D col)
     {
-        Debug.Log("Duele");
         if (col.gameObject.tag == "Player")
         {
             col.gameObject.transform.position = new Vector3 (-7.92f, -26.6f, 0);
