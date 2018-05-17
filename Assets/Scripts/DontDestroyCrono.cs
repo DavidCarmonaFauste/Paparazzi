e14fe6ce61ed40a7ed324f5e2b2ed479aca1dbc5
pablojor <pablojor@ucm.es>
2018-05-17 16:34:41 +0200

    Problema con camaras arreglado

---
+++ Assets/Scripts/DontDestroyCrono.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyCrono : MonoBehaviour {
+
+    public static DontDestroyCrono instance = null;
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else Destroy(this.gameObject);
+    }
+}
