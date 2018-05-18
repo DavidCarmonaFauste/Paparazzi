73fc18ca130b7a833c9ad1d6df9e861a68118587
DavidCarmonaFauste <davicarm@ucm.es>
2018-05-19 00:07:52 +0200

    Animaciones, escala del canvas corregidas en minijuegos

--- Assets/Scripts/Minijuego/MovRandom3.cs
+++ Assets/Scripts/Minijuego/MovRandom3.cs
@@ -2,21 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovRandom2 : MonoBehaviour
+public class MovRandom3 : MonoBehaviour
 {
 
     private Rigidbody2D rb2d;
     public float MovX; //test
     public float MovY; //test
     public float V;
-    Animator animacion;
 
     void Start()
     {
         MovX = Random.Range(1, 3);
         MovY = Random.Range(1, 3);
         rb2d = GetComponent<Rigidbody2D>();
-        animacion = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -24,9 +22,9 @@ public class MovRandom2 : MonoBehaviour
         Vector2 mov = new Vector2(MovX, MovY);
         rb2d.velocity = (mov) * V;
 
-        if (MovX < 0)
-            animacion.SetTrigger("izquierda");
-        else animacion.SetTrigger("derecha");
+       /* if (MovX < 0)
+            transform.localScale = new Vector3(1f, 0, 0);
+        else transform.localScale = new Vector3(-1f, 0, 0);*/
 
     }
 
